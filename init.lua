e526ebb939a9dbe912a3bb6aa5a81667b1f3a49d
Ryker Sun <rykersun2002@gmail.com>
2022-08-01 00:21:31 +0800

    add discord plugin

--- init.lua
+++ init.lua
@@ -103,7 +103,7 @@ local config = {
       -- ["goolord/alpha-nvim"] = { disable = true },
 
       -- You can also add new plugins here as well:
-      -- { "andweeb/presence.nvim" },
+      { "andweeb/presence.nvim" },
       -- {
       --   "ray-x/lsp_signature.nvim",
       --   event = "BufRead",
